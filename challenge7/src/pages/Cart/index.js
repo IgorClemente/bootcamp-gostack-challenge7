23a34e185161d5957eb0ccb512f05be37a0769d5
Igor Clemente <igorelectrofest2011@>
2019-10-04 09:19:24 -0300

    BOOTCAMP_GOSTACK_MODULE8_CHALLENGE7_CARTCOMPONENT_UPDATE_04102019

--- challenge7/src/pages/Cart/index.js
+++ challenge7/src/pages/Cart/index.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
 
-import { Container, Products } from './styles';
+import {
+  Container,
+  Products,
+  Product,
+  ProductImage,
+  ProductTitle,
+  ProductPrice,
+} from './styles';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default class Home extends Component {
+  state = {
+    cart: [],
+  };
+
   render() {
+    const { cart } = this.state;
+    const { product } = this.props;
+
     return (
       <Container>
-        <Products>
+        <Products data={product} keyExtractor={item => item.id}>
           <Product>
-            <ProductImage></ProductImage>
-            <ProductTitle></ProductTitle>
-            <ProductPrice></ProductPrice>
-            <Icon name="" />
+            <ProductImage
+              source={{ uri: '../../assets/teste/tenis1.jpg' }}
+            ></ProductImage>
+            <ProductTitle>Produto de teste</ProductTitle>
+            <ProductPrice>R$120,00</ProductPrice>
+            <Icon name="delete-forever" color="#fff" size={24} />
           </Product>
         </Products>
       </Container>
--- challenge7/src/pages/Cart/styles.js
+++ challenge7/src/pages/Cart/styles.js
@@ -5,6 +5,35 @@ export const Container = Styled.View`
   padding: 12px;
   margin: 20px;
   border-radius: 4px;
+  justify-content: center;
 `;
 
-export const Products = Styled.FlatList``;
+export const Products = Styled.FlatList`
+  padding: 12px;
+  margin: 12px;
+  background: #333;
+`;
+
+export const Product = Styled.View`
+  flex: 1;
+  background: #333;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ProductImage = Styled.Image`
+  flex: 1;
+  width: 100px;
+  height: 100px;
+`;
+
+export const ProductTitle = Styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+export const ProductPrice = Styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  color: #999;
+`;
